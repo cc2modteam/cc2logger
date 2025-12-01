8143cc6cffa26eafa987c4c5a6159eb40f8c0eba
Ian Norton <inorton@gmail.com>
2025-12-01 22:35:07 +0000

    Tweaks to keep logs in the volume

--- run-cc2-docker.sh
+++ run-cc2-docker.sh
@@ -6,4 +6,4 @@ IMAGE=bredlab/cc2-server:stable
 docker rm -f cc2-${NAME} || true
 docker run \
   -p 25565:25565/udp -p 25566:25566/udp -p 25567:25567/udp -p 25568:25568/udp \
-  --rm --name cc2-${NAME} -v carriercommand:/carriercommand -id ${IMAGE}
+  --rm --name cc2-${NAME} -v carriercommand:/logs -v carriercommand:/carriercommand -id ${IMAGE}
